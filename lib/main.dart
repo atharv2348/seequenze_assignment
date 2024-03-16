65e40612f7eb52187c517a32dce03095934f6266
atharv <atharvc2022@gmail.com>
2024-03-16 18:15:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:seequenze_assignment/pages/detail_page.dart';
+import 'package:seequenze_assignment/pages/home_page.dart';
+import 'package:seequenze_assignment/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(430, 930),
+      builder: (_, chlid) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            fontFamily: 'inter',
+            useMaterial3: true,
+          ),
+          home:  HomePage(),
+        );
+      },
+    );
+  }
+}
